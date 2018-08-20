3cbf7a866f2bb59ad2963d40fb022c8b9336d301
mark <m.arklee@outlook.com>
2018-08-20 15:39:27 -0700

    Link css with D3 appended tags

--- js/life-expectancy.js
+++ js/life-expectancy.js
@@ -1,10 +1,16 @@
 let margin = {left: 100, right: 10, top: 10, bottom: 100};
-let width = 600 - margin.left - margin.right;
-let height = 400 - margin.top - margin.bottom; 
+let width = 800 - margin.left - margin.right;
+let height = 600 - margin.top - margin.bottom; 
 
 let svg = d3.select("#chart-area")
 	.append("svg")
 		.attr("width", width + margin.left + margin.right)
+		.attr("height", height + margin.top + margin.bottom);
+
+
 d3.json("data/data.json").then(function(data){
-	console.log(data);
-})
+	
+
+
+
+});
