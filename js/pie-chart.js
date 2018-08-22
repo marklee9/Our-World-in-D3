6fadd5979f6150e4bc0f854e0fcfdc1c98e49841
mark <m.arklee@outlook.com>
2018-08-22 10:09:25 -0700

    Trying different apporach to render pie chart

--- js/pie-chart.js
+++ js/pie-chart.js
@@ -2,50 +2,73 @@ const pieChart = () => {
   let width = 800;
   let height = 800;
   let radius = 400;
+  let color = d3.scaleOrdinal(d3.schemePastel1);
 
-let arc = d3.arc()
-  .outerRadius(radius)
-  .innerRadius(100)
-  .startAngle(0)
-  .endAngle(Math.PI / 2);
+  let chart = d3.select("#pie-chart-area")
+    .append("svg")
+      .attr("width", width)
+      .attr("height", height)
+      .style("background", "pink");
 
-let pie = d3.pie()
-  .value(d => d.population)
-  .sort(null);
+  d3.json("data/main-data.json").then((data) => {
+    let firstData = data[0].byContinents.map((d)=> {
+      d.population = Number(d.population);
+      return d;
+    });
 
-let pieChart = d3.select("#pie-chart-area").append("svg")
-    .attr("width", width)
-    .attr("height", height)
-  .append("g")
-    .attr("transform", "translate(400, 400)");
+    console.log(firstData);
 
-const type = (d) => {
-  d.population = Number(d.population);
-  return d;
-};
-
-let color = d3.scaleOrdinal(d3.schemePastel1);
-
-d3.json("data/main-data.json", type).then((data) => {
-  data[0].byContinents.forEach((datum)=> 
-   updatePie(datum)
-  );
-});
-
-
-const updatePie = (datum) => {
-    // Joining Data
-    let path = pieChart.selectAll(".path")
-      .data(pie(datum.population));
-
-  	path.exit().remove();
-
-    // Enter new data
-    path.enter().append("path")
-      .attr("class", "path")
-      .attr("fill", (d) => color(d))
-      .attr("d", arc);
-  };
+  });
+  
 };
 
 pieChart();
+
+
+  // let width = 800;
+  // let height = 800;
+  // let radius = 400;
+
+  // let arc = d3.arc()
+  //   .outerRadius(radius)
+  //   .innerRadius(100)
+  //   .startAngle(0)
+  //   .endAngle(Math.PI / 2);
+
+  // let pie = d3.pie()
+  //   .value(d => d.population)
+  //   .sort(null);
+
+  // let pieChart = d3.select("#pie-chart-area").append("svg")
+  //   .attr("width", width)
+  //   .attr("height", height)
+  //   .append("g")
+  //   .attr("transform", "translate(400, 400)");
+
+  // const type = (d) => {
+  //   d.population = Number(d.population);
+  //   return d;
+  // };
+
+  // let color = d3.scaleOrdinal(d3.schemePastel1);
+
+  // d3.json("data/main-data.json", type).then((data) => {
+  //   data[0].byContinents.forEach((datum) =>
+  //     updatePie(datum)
+  //   );
+  // });
+
+
+  // const updatePie = (datum) => {
+  //   // Joining Data
+  //   let path = pieChart.selectAll(".path")
+  //     .data(pie(datum.population));
+
+  //   path.exit().remove();
+
+  //   // Enter new data
+  //   path.enter().append("path")
+  //     .attr("class", "path")
+  //     .attr("fill", (d) => color(d))
+  //     .attr("d", arc);
+  // };
