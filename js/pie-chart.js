f0603e8997c1b1816864aea5c24b94f851e34c0a
mark <m.arklee@outlook.com>
2018-08-21 17:46:23 -0700

    Add pie

--- js/pie-chart.js
+++ js/pie-chart.js
@@ -3,12 +3,18 @@ height = 800;
 let radius = 400;
 
 let arc = d3.arc()
-  .outerRadius(radius - 10)
+  .outerRadius(radius)
   .innerRadius(100);
 
-let pie = d3.select("#pie-chart-area").append("svg")
+let pie = d3.pie()
+  .value(d => d.population)
+  .sort(null);
+
+let pieChart = d3.select("#pie-chart-area").append("svg")
   .attr("width", width)
   .attr("height", height)
   .append("g")
     .attr("background-color", "pink")
     .attr("transform", "translate(400, 400)");
+
+d3.json("data/data.json").then((data) => { });
