7161b3c4a111a22078cf79fb112ead6705872d8a
mark <m.arklee@outlook.com>
2018-08-22 21:32:12 -0700

    Correctly renders legend of the pie chart

--- js/pie-chart.js
+++ js/pie-chart.js
@@ -1,9 +1,11 @@
 const pieChart = () => {
-  let width = 800;
+  // Constants
+  let width = 1000;
   let height = 800;
   let radius = 400;
   let color = d3.scaleOrdinal(d3.schemePastel1);
 
+  // Selecting div
   let chart = d3.select("#pie-chart-area")
     .append("svg")
       .attr("width", width)
@@ -15,6 +17,7 @@ const pieChart = () => {
       return d;
     });
 
+    // taking each data to invoke update function.
     update(dataOfInterest);
   });
 
@@ -23,6 +26,7 @@ const pieChart = () => {
       .sort(null)
       .value(d => d.population)(data);
   
+    // All Pie chart property
     let arcs = d3.arc()
       .innerRadius(100)
       .outerRadius(300)
@@ -40,8 +44,24 @@ const pieChart = () => {
       .attr("fill", (d) => color(d.data.continent));
 
     let legends = chart.append("g")
-      .attr("transform", "translate(600, 600)")
-      .selectAll(".legends").data(pie);
+      .attr("transform", "translate(750, 500)")
+      .selectAll(".legends")
+      .data(pie);
+    
+    let legend = legends.enter()
+      .append("g")
+      .classed("legends", true)
+      .attr("transform", (d,i) => "translate(0, " + ((i + 1) * 30) + ")");
+      
+    legend.append("rect")
+      .attr("width", 20)
+      .attr("height", 20)
+      .attr("fill", (d) => color(d.data.continent));
+
+    legend.append("text")
+      .attr("x", 30)
+      .attr("y", 15)
+      .text((d) => d.data.continent);
   };
 };
 
