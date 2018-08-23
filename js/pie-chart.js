8abe6a875625355cf3b48a90ef9164af998af784
mark <m.arklee@outlook.com>
2018-08-22 23:56:40 -0700

    Implement pie mouseover and mouseleave functions

--- js/pie-chart.js
+++ js/pie-chart.js
@@ -11,15 +11,17 @@ const pieChart = () => {
       .attr("width", width)
       .attr("height", height);
 
-  d3.json("data/main-data.json").then((data) => {
-    let dataOfInterest = data[0].byContinents.map((d)=> {
-      d.population = Number(d.population);
-      return d;
-    });
-
-    // taking each data to invoke update function.
-    update(dataOfInterest);
+d3.json("data/main-data.json").then((data) => {
+  let dataOfInterest = data[0].byContinents.map((d)=> {
+    d.population = Number(d.population);
+    return d;
   });
+
+  // taking each data to invoke update function.
+  update(dataOfInterest);
+});
+
+
 
   const update = (data) => {
     let pie = d3.pie()
@@ -33,6 +35,14 @@ const pieChart = () => {
       .padAngle(0.1)
       .padRadius(50)
       .cornerRadius(20);
+
+    // Hover effect on each slice
+    let arcHover = d3.arc()
+        .innerRadius(280)
+        .outerRadius(400)
+        .padAngle(0.1)
+        .padRadius(50)
+        .cornerRadius(100);
   
     // Pie slice
     let sections = chart.append("g")
@@ -40,9 +50,21 @@ const pieChart = () => {
       .selectAll("path")
       .data(pie);
   
-    sections.enter().append("path")
-      .attr("d", (d) => arcs(d))
-      .attr("fill", (d) => color(d.data.continent));
+      sections.enter().append("path")
+        .attr("d", (d) => arcs(d))
+        .attr("fill", (d) => color(d.data.continent))
+        .on("mouseover", function(d){
+          console.log(d);
+          d3.select(this)
+            .transition()
+            .duration(200)
+            .attr("d", (d2) => arcHover(d2));})
+        .on("mouseleave", function(){
+          d3.select(this)
+            .transition()
+            .duration(200)
+            .attr("d", (d2) => arcs(d2));
+        });
 
     // Legends
     let legends = chart.append("g")
@@ -50,20 +72,20 @@ const pieChart = () => {
       .selectAll(".legends")
       .data(pie);
     
-    let legend = legends.enter()
-      .append("g")
-      .classed("legends", true)
-      .attr("transform", (d,i) => "translate(0, " + ((i + 1) * 30) + ")");
-      
-    legend.append("rect")
-      .attr("width", 20)
-      .attr("height", 20)
-      .attr("fill", (d) => color(d.data.continent));
+      let legend = legends.enter()
+        .append("g")
+        .classed("legends", true)
+        .attr("transform", (d,i) => "translate(0, " + ((i + 1) * 30) + ")");
+        
+      legend.append("rect")
+        .attr("width", 20)
+        .attr("height", 20)
+        .attr("fill", (d) => color(d.data.continent));
 
-    legend.append("text")
-      .attr("x", 30)
-      .attr("y", 15)
-      .text((d) => d.data.continent);
+      legend.append("text")
+        .attr("x", 30)
+        .attr("y", 15)
+        .text((d) => d.data.continent);
   };
 };
 
